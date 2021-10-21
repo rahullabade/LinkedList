7cc4d26b1b3b7c9b8a710a1e9325811ca4f6bccd
RAHUL LABADE <rahullabade05@gmail.com>
2021-10-21 09:38:27 +0530

    Added: StackOperation

---
+++ MyStack.java
@@ -0,0 +1,24 @@
+package LinkList;
+
+import LinkListPractice.INode;
+import LinkListPractice.MyLinkList;
+
+public class MyStack {
+	private final MyLinkList myLinkList;
+
+	public MyStack() {
+		this.myLinkList = new MyLinkList();
+	}
+
+	public void push(INode myNode) {
+		myLinkList.add(myNode);
+	}
+
+	public void printStack() {
+		myLinkList.printMyNode();
+	}
+
+	public INode peak() {
+		return myLinkList.head;
+	}
+}
---
+++ MyStackTest.java
@@ -0,0 +1,24 @@
+package LinkList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import LinkListPractice.INode;
+import LinkListPractice.MyNode;
+
+public class MyStackTest {
+	@Test
+
+	public void given3NumberAddedToStackShouldHaveLastAddedNode() {
+		MyNode<Integer> myFirstNode = new MyNode<>(70);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		MyStack myStack = new MyStack();
+		myStack.push(myFirstNode);
+		myStack.push(mySecondNode);
+		myStack.push(myThirdNode);
+		myStack.printStack();
+		INode peak = myStack.peak();
+		Assert.assertEquals(myThirdNode, peak);
+	}
+}
